4f3e3c309c8d869f60b8fdeb4b7845872eba9e81
jisungbin <ji@sungb.in>
2021-09-10 21:23:44 +0900

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = Application.compileSdk
+
+    defaultConfig {
+        minSdk = Application.minSdk
+        targetSdk = Application.targetSdk
+        versionCode = Application.versionCode
+        versionName = Application.versionName
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+        }
+    }
+
+    sourceSets {
+        getByName("main").run {
+            java.srcDirs("src/main/kotlin")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = Application.sourceCompat
+        targetCompatibility = Application.targetCompat
+    }
+
+    kotlinOptions {
+        jvmTarget = Application.jvmTarget
+    }
+}
+
+dependencies {
+    Dependencies.essential.forEach(::implementation)
+    Dependencies.compose.forEach(::implementation)
+    Dependencies.ui.forEach(::implementation)
+}
