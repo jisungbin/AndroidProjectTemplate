ac24f3ec874c6394030002aad7d7e45ce58f33fb
jisungbin <ji@sungb.in>
2022-06-22 05:00:10 +0900

    Remove JaCoCo

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -3,25 +3,6 @@ plugins {
     id("kotlin-android")
     id("org.jetbrains.dokka") version Versions.BuildUtil.Dokka
     id("name.remal.check-dependency-updates") version Versions.BuildUtil.CheckDependencyUpdates
-    jacoco
-}
-
-jacoco {
-    toolVersion = Versions.Test.JaCoCo
-}
-
-tasks.withType<JacocoReport> {
-    reports {
-        html.required.set(true)
-        html.outputLocation.set(layout.projectDirectory.dir("../documents/coverage/jacoco/html"))
-        xml.required.set(true) // codecov depends on xml format report
-        xml.outputLocation.set(layout.projectDirectory.file("../documents/coverage/jacoco/report.xml"))
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    finalizedBy("jacocoTestReport")
 }
 
 android {
